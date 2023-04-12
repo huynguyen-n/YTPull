90e474f9bc035cc5885dc7490c497b8819f9b426
Huy Nguyen <huy.ng1012@gmail.com>
2023-04-12 21:48:43 +0700

    feature(ui): finialize get video info and added to the list

--- YTPull/Sources/View/ContentView.swift
+++ YTPull/Sources/View/ContentView.swift
@@ -9,32 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
 
-    var colors = ["Red", "Green", "Blue", "Tartan"]
-    @State private var url = ""
-    @State private var selectedColor = ""
+    @State private var url: String = "https://www.youtube.com/watch?v=tPEE9ZwTmy0"
+    @State private var videos: [VideoInfo] = []
 
     var body: some View {
         VStack {
             TextField("https://www.youtube.com", text: $url)
             Button("Get Video Info", action: didTapGetURL).disabled(!url.isValid)
+            List(videos) { video in
+                VideoRow(video: video)
+            }
+            .frame( maxWidth: .infinity)
+            .edgesIgnoringSafeArea(.all)
         }
         .padding()
     }
 
-    private func didSelectPicker() {
-        print("hello")
-    }
-
     private func didTapGetURL() {
-        guard url.isValid else { return }
         do {
             let data = try Execute.shared.excute(["-f", "bv/ba", "-j", url])
-            let videoInfo = try JSONDecoder().decode(VideoInfo.self, from: data)
-            print(videoInfo)
+            let video = try JSONDecoder().decode(VideoInfo.self, from: data)
+            videos.append(video)
+            url = ""
         } catch {
             print(error)
         }
-
     }
 }
 
